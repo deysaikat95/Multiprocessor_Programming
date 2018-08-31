d982b026c16e696d665404427c98f6c02a1246ec
Mincheol Sung <mincheol@vt.edu>
2018-08-31 01:09:27 -0400

    Implement ADD, BUY, SELL

--- Assignment1/src/hw1/Database.java
+++ Assignment1/src/hw1/Database.java
@@ -12,21 +12,18 @@ public class Database {
 	private static String outputFile;
 	private static String inputFile;
 
+	private ArrayList<String[]> database;
+	private int num_entry;
+
 	Database(String inputFile, String outputFile)
 	{
 		this.inputFile = inputFile;
 		this.outputFile = outputFile;
-	}
-
-	ArrayList<String[]> database;
-	int num_entry;
-
-	Database() {
 		this.database = new ArrayList<String[]>();
 		num_entry = 0;
 	}
 
-	public static boolean isValidDate(String inDate) {
+	private static boolean isValidDate(String inDate) {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
     	dateFormat.setLenient(false);
     	try {
@@ -37,15 +34,25 @@ public class Database {
     		return true;
 	}
 
-	public static void printStringArr(String[] stringArr) {
+	private int internalSearch (String name, String company) {
 		try {
-			System.out.print("#");
-			for (int i=0; i<stringArr.length; i++)
-				System.out.print(stringArr[i] + "|");
+			String[] temp;
 
-			System.out.println("#");
+			if (this.num_entry == 0)
+				return -1;
+
+			ListIterator iterator = this.database.listIterator();
+			while (iterator.hasNext()) {
+				temp = (String[])iterator.next();
+				if (temp[0].equals(name) && temp[1].equals(company)) {
+					return this.database.indexOf(temp);
+				}
+			}
+
+			return -1;
 		} catch (Exception e) {
-			System.out.println(e + " in printStringArr method");
+			System.out.println(e + " in internalSearch method");
+			return -2;
 		}
 	}
 
@@ -88,24 +95,17 @@ public class Database {
 		}
 	}
 
-	public void add(String[] data, boolean quotation1, boolean quotation2) {
+	public void add(String[] data) {
 		try {
 			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(outputFile), true));
 
-			/* DUPBLICATE ERROR HANDLING */
-			if (internalSearch(data[1],data[2]) != -1) {
+			if (internalSearch(data[0],data[1]) != -1) {
 				printWriter.append("ADD: ERROR DUPLICATE_ENTRY\n");
 				printWriter.close();
 				return;
 			}
 
-			if (!data[3].equals("true") && !data[3].equals("false")) {
-				printWriter.append("ADD: ERROR NOT_BOOL\n");
-				printWriter.close();
-				return;
-			}
-
-			if (!Database.isValidDate(data[0])) {
+			if (!Database.isValidDate(data[2])) {
 				printWriter.append("ADD: ERROR INVALID_DATE\n");
 				printWriter.close();
 				return;
@@ -115,23 +115,26 @@ public class Database {
 			this.num_entry = this.database.size();
 
 			printWriter.append("ADD: OK ");
-			if (quotation1)
-				printWriter.append("\"");
 
-			printWriter.append(data[1]);
-
-			if (quotation1)
+			if(data[0].contains(" "))
+			{
 				printWriter.append("\"");
+				printWriter.append(data[0]);
+				printWriter.append("\"");
+			}
+			else
+				printWriter.append(data[0]);
 
 			printWriter.append(" ");
 
-			if (quotation2)
+			if (data[1].contains(" "))
+			{
 				printWriter.append("\"");
-
-			printWriter.append(data[2]);
-
-			if (quotation2)
+				printWriter.append(data[1]);
 				printWriter.append("\"");
+			}
+			else
+				printWriter.append(data[1]);
 
 			printWriter.append("\n");
 
@@ -140,6 +143,118 @@ public class Database {
 			System.out.println(e + " in add method in Database.java");
 		}
 	}
+
+	public void buy(String[] data) {
+		try {
+			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(outputFile), true));
+
+			/* DUPBLICATE ERROR HANDLING */
+			int index = internalSearch(data[0], data[1]);
+			if (index < 0) {
+				printWriter.append("BUY: ERROR CANNOT_BUY_BEFORE_ADD\n");
+				printWriter.close();
+				return;
+			}
+			String[] temp = this.database.get(index);
+
+			int quantity = Integer.parseInt(temp[3]);
+			quantity += Integer.parseInt(data[2]);
+
+			temp[3] =  Integer.toString(quantity);
+
+			printWriter.append("BUY: OK ");
+
+			if(data[0].contains(" "))
+			{
+				printWriter.append("\"");
+				printWriter.append(data[0]);
+				printWriter.append("\"");
+			}
+			else
+				printWriter.append(data[0]);
+
+			printWriter.append(" ");
+
+			if (data[1].contains(" "))
+			{
+				printWriter.append("\"");
+				printWriter.append(data[1]);
+				printWriter.append("\"");
+			}
+			else
+				printWriter.append(data[1]);
+
+			printWriter.append(" ");
+
+			printWriter.append(temp[3]);
+
+			printWriter.append("\n");
+
+			printWriter.close();
+		} catch (Exception e) {
+			System.out.println(e + " in buy method in Database.java");
+		}
+	}
+
+	public void sell(String[] data) {
+		try {
+			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(outputFile), true));
+
+			/* DUPBLICATE ERROR HANDLING */
+			int index = internalSearch(data[0], data[1]);
+			if (index < 0) {
+				printWriter.append("SELL: ERROR CANNOT_SELL_BEFORE_ADD\n");
+				printWriter.close();
+				return;
+			}
+			String[] temp = this.database.get(index);
+
+			int quantity = Integer.parseInt(temp[3]);
+			quantity -= Integer.parseInt(data[2]);
+			if (quantity < 0)
+			{
+				printWriter.append("SELL: ERROR CANNOT_SELL_QUANTITY_MORE_THAN_STOCK\n");
+				printWriter.close();
+				return;
+
+			}
+
+			temp[3] =  Integer.toString(quantity);
+
+			printWriter.append("SELL: OK ");
+
+			if(data[0].contains(" "))
+			{
+				printWriter.append("\"");
+				printWriter.append(data[0]);
+				printWriter.append("\"");
+			}
+			else
+				printWriter.append(data[0]);
+
+			printWriter.append(" ");
+
+			if (data[1].contains(" "))
+			{
+				printWriter.append("\"");
+				printWriter.append(data[1]);
+				printWriter.append("\"");
+			}
+			else
+				printWriter.append(data[1]);
+
+			printWriter.append(" ");
+
+			printWriter.append(temp[3]);
+
+			printWriter.append("\n");
+
+			printWriter.close();
+		} catch (Exception e) {
+			System.out.println(e + " in sell method in Database.java");
+		}
+	}
+
 
 	public void store(String name) {
 		try {
@@ -155,19 +270,20 @@ public class Database {
 
 	public void status() {
 		String[] temp;
-		int count = 0;
 		try {
 			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(outputFile), true));
+			printWriter.append("STATUS: OK " + this.database.size() + "\n");
+
 			ListIterator iterator = this.database.listIterator();
-			while (iterator.hasNext()) {
+			while (iterator.hasNext())
+			{
 				temp = (String[])iterator.next();
-				printWriter.append(temp[1] + "," + temp[2] + "," + temp[0] + "\n");
-				count++;
+				printWriter.append(temp[0] + "," + temp[1] + "," + temp[2] + "," + temp[3] + "\n");
 			}
-			printWriter.append("STATUS: OK " + count + "\n");
+
 			printWriter.close();
 		} catch (Exception e) {
-			System.out.println(e + " in show method");
+			System.out.println(e + " in status method");
 		}
 	}	
 
@@ -194,129 +310,6 @@ public class Database {
 			printWriter.close();
 		} catch (Exception e) {
 			System.out.println(e + " in search method");
-		}
-	}
-
-	public int internalSearch (String title, String director) {
-		try {
-			String[] temp;
-			ListIterator iterator = this.database.listIterator();
-			while (iterator.hasNext()) {
-				temp = (String[])iterator.next();
-				if (temp[1].equals(title) && temp[2].equals(director)) {
-					return this.database.indexOf(temp);
-				}
-			}
-
-			return -1;
-		} catch (Exception e) {
-			System.out.println(e + " in internalSearch method");
-			return -2;
-		}
-	}
-
-/*
-	String[] data's form = {"Third argument of commend. (EX. true or 01/01/2016)","Title","Director"}
-*/
-	public void update(String[] data, boolean quotation1, boolean quotation2) 
-	{
-		try {
-			PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(outputFile), true));
-			/* update old {Title, Director} to new {Title, Director} */
-			if (!data[2].equals("true") && !data[2].equals("false") && !data[2].contains("/")) {
-				//System.out.println("gotcha");
-				int offset = internalSearch(data[0], data[1]);
-
-				if (offset == -1) {
-					printWriter.append("UPDATE: ERROR FILM_NOT_FOUND\n");
-					printWriter.close();
-					return;
-				}
-				//Database.printStringArr(data);
-
-				/* DUPBLICATE ERROR HANDLING */
-				if (internalSearch(data[2],data[3]) != -1) {
-					printWriter.append("UPDATE: ERROR DUPLICATE_ENTRY\n");
-					printWriter.close();
-					return;
-				}
-
-
-				String[] temp = (String[])this.database.get(offset);
-
-				temp[1] = data[2];
-				temp[2] = data[3];
-
-				printWriter.append("UPDATE : OK ");
-				if (data[2].contains(" "))
-					printWriter.append("\"");
-
-				printWriter.append(data[2]);
-
-				if (data[2].contains(" "))
-					printWriter.append("\"");
-
-				printWriter.append(" ");
-
-				if (data[3].contains(" "))
-					printWriter.append("\"");
-
-				printWriter.append(data[3]);
-
-				if (data[3].contains(" "))
-					printWriter.append("\"");
-
-				printWriter.append("\n");
-
-			} else {
-
-
-				// System.out.println("gotcha222");
-				int offset = internalSearch(data[0], data[1]);
-				if (offset == -1) {
-					printWriter.append("UPDATE: ERROR FILM_NOT_FOUND\n");
-					printWriter.close();
-					return;
-				}
-				// System.out.println(offset);
-				String[] temp = (String[])this.database.get(offset);
-				if (data[2].equals("true") || data[2].equals("false")) {
-					temp[3] = data[2];
-				} else if (Database.isValidDate(data[2])) {
-					temp[0] = data[2];
-				} 
-				/* ERROR HANDLING */
-				else {
-					printWriter.append("UPDATE: INVALID_DATE_OR_BOOL\n");
-					printWriter.close();
-					return;
-				}
-
-				printWriter.append("UPDATE : OK ");
-				if (quotation1)
-					printWriter.append("\"");
-
-				printWriter.append(data[0]);
-
-				if (quotation1)
-					printWriter.append("\"");
-
-				printWriter.append(" ");
-
-				if (quotation2)
-					printWriter.append("\"");
-
-				printWriter.append(data[1]);
-
-				if (quotation2)
-					printWriter.append("\"");
-
-				printWriter.append("\n");
-				
-			}
-			printWriter.close();
-		} catch (Exception e) {
-			System.out.println(e + " in update method in Database.java");
 		}
 	}
 }
